4b169d73b0324bd866e5522323ff72094de7a635
Vlad <student@205-11>
2019-05-28 14:41:47 +0300

    Восстановление пароля

--- Web/Controllers/HomeControllerPart2.cs
+++ Web/Controllers/HomeControllerPart2.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Web.Controllers
 {
     public partial class HomeController
     {
+        [HttpPost]
+        public async Task<ActionResult> SendCode(string loginOrEmail)
+        {
+            return PartialView("PartialSendCode");
+        }
     }
 }
