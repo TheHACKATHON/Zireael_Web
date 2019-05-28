604a348a69a280cbc8f44558e6c2e761aa0f1fe7
Никита <okorokmahjonga@gmail.com>
2019-05-28 09:59:01 +0300

    Сортировал

---
+++ Web/App_Start/CustomControllerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
+using Web.Controllers;
+
+namespace Web
+{
+    public class CustomControllerFactory : IControllerFactory
+    {
+        public IController CreateController(RequestContext requestContext, string controllerName)
+        {
+             return new HomeController(Clients.Get(requestContext.HttpContext.Session.SessionID));
+        }
+
+        public SessionStateBehavior GetControllerSessionBehavior(RequestContext requestContext, string controllerName)
+        {
+            return SessionStateBehavior.Required;
+        }
+
+        public void ReleaseController(IController controller)
+        {
+            if (controller is IDisposable disposable)
+                disposable.Dispose();
+            else
+                controller = null;
+        }
+    }
+}
---
+++ Web/Models/Clients.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using Web.Controllers;
+using Web.ServiceReference1;
+
+namespace Web
+{
+    public static class Clients
+    {
+        private static Dictionary<string, CeadChatServiceClient> _instanse;
+
+
+        static Clients()
+        {
+            _instanse = new Dictionary<string, CeadChatServiceClient>();
+        }
+
+        public static CeadChatServiceClient Add(string sessionId)
+        {
+            var client = new CeadChatServiceClient(new InstanceContext(new HomeController(null)));
+            client.Endpoint.Binding.SendTimeout = new TimeSpan(1, 0, 0);
+            client.Open();
+            _instanse.Add(sessionId, client);
+            return _instanse[sessionId];
+        }
+
+        public static void Remove(string sessionId)
+        {
+            _instanse.Remove(sessionId);
+        }
+
+        public static CeadChatServiceClient Get(string sessionId)
+        {
+            return _instanse.ContainsKey(sessionId) ? _instanse[sessionId] : Add(sessionId);
+        }
+    }
+}
