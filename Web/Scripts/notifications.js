1603ff5ea0160464178469bb6316e005591435ef
Никита <okorokmahjonga@gmail.com>
2019-05-29 11:10:01 +0300

    notify

---
+++ Web/Scripts/notifications.js
@@ -0,0 +1,18 @@
+﻿function popup(string, status) {// success, notice, warning, error
+    var type = "bottom-right";
+
+    if (!$('.notify').hasClass('do-show')) {
+        $('.notify')
+            .text(string)
+            .removeClass()
+            .attr('data-notification-status', status)
+            .addClass(type + ' notify')
+            .addClass('do-show');
+        setTimeout(() => { $('.notify').removeClass('do-show').text(""); }, 6000);
+
+        event.preventDefault();
+    }
+    else {
+        setTimeout(() => { popup(string, status ) }, 1000);
+    }
+}
