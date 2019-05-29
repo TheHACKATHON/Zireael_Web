ed533f888b7a7a49c1f91598a70e05cd9bd7ee4f
Никита <okorokmahjonga@gmail.com>
2019-05-29 11:17:30 +0300

    string generator

---
+++ Web/Models/Generator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ASP_Articles.Models
+{
+    public class Generator
+    {
+        private static string BytesToHexString(byte[] bytes)
+        {
+            var hexString = new StringBuilder(64);
+
+            foreach (var b in bytes)
+            {
+                hexString.Append($"{b:X2}");
+            }
+
+            return hexString.ToString();
+        }
+
+        public static string String(int size)
+        {
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                size = (int) Math.Ceiling(size / 2.0);
+                var salt = new byte[size];
+                generator.GetBytes(salt);
+                return BytesToHexString(salt);
+            }
+        }
+    }
+}
